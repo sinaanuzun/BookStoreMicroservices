f503fb7eeabb5e6c3336de209cbed5314eefc9bb
sinaanuzun <sinanuzun43@gmail.com>
2023-10-08 02:22:33 +0300

    Initial commit

--- book-service/src/main/java/com/example/bookservice/repository/BooRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.bookservice.repository;
-
-import com.example.bookservice.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BooRepository extends JpaRepository<Book, String> {
-}
