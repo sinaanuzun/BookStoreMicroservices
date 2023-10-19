ac11e009de5ef9b59f34c697ac56a7dd332570f1
sinaanuzun <sinanuzun43@gmail.com>
2023-10-20 02:44:03 +0300

    update

--- library-service/src/main/java/com/example/libraryservice/repository/LibraryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.libraryservice.repository;
-
-import com.example.libraryservice.model.Library;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LibraryRepository extends JpaRepository<Library, String> {
-}
