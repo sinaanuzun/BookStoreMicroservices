7b6a51b25778bbd122767da3226283ec92aa4ce6
sinaanuzun <sinanuzun43@gmail.com>
2023-11-06 16:11:05 +0300

    update

--- book-service/src/main/java/com/example/bookservice/exception/GlobalExceptionHandler.java
+++ book-service/src/main/java/com/example/bookservice/exception/GlobalExceptionHandler.java
@@ -7,9 +7,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(IdNotFoundException.class)
     public ResponseEntity<String> handleIdNotFoundException(IdNotFoundException ex) {
